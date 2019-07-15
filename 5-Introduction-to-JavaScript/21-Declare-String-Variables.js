b53c6af9030c7ed3ccc37b30b128eea20404af29
Rafna <rafnabash@gmail.com>
2019-07-15 00:12:49 -0600

    Declare string variable

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+//Declare string variable using double quotes
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Rafna";
+var myLastName = "Bash";
