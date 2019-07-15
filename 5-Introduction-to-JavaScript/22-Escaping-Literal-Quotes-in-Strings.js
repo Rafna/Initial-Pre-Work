d91bf94eaacc94a84b6a3e5fcf203c1018c8bea3
Rafna <rafnabash@gmail.com>
2019-07-15 00:17:53 -0600

    Use backslash in string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+/* escape a quote from considering it as an end of string quote by
+using backslash \ in front of the quote. */
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
