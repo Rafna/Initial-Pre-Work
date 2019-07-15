bb76d53ac9e55029640f5bcf1fdd55f1e0984a52
Rafna <rafnabash@gmail.com>
2019-07-15 16:54:54 -0600

    Show javascript object with properties

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+/* Objects are useful for storing data in a structured way, and can
+represent real world objects. */
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Morgan",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Me","Bill"]
+};
