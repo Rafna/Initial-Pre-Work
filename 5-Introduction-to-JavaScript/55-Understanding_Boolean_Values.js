6ab37fd65a0ecfd9df3b9df6dd2a6cd34b5567c2
Rafna <rafnabash@gmail.com>
2019-07-15 02:13:42 -0600

    Show boolean values

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//Boolean is another data type which has one of two values: true or false.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
