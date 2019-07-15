a2917c4490ae4f8a8fa8eef22a979d22814579d9
Rafna <rafnabash@gmail.com>
2019-07-14 23:48:51 -0600

    Create decimal number

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,6 @@
+/* Decimal numbers are referred as floating point or floats. Can store Decimal
+numbers in variables. */
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 3.6;
