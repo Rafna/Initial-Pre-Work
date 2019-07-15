62269d2a01c36a4591d8fdf1e55e26d65f92e2ca
Rafna <rafnabash@gmail.com>
2019-07-15 00:03:13 -0600

    Use mathematical operator and assignment operator in one step

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,12 @@
+/* As it is common to use assignment operator to modify contents of a variable,
+there are operators which do both mathematical operation and assignment in one step.
+*/
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
