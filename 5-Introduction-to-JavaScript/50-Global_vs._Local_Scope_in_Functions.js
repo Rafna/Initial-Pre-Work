2c9ddea04f6c3fc4b4c398ed85f6eb507079becb
Rafna <rafnabash@gmail.com>
2019-07-15 01:57:29 -0600

    Show local variable precede over global variable

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+/*Local variable with same name as global varaibale has precedence over
+global variable. */
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
