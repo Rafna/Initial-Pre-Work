c6b62b953e17444eba2a572ebfc042f68bf527d8
Rafna <rafnabash@gmail.com>
2019-07-15 15:42:48 -0600

    Use else if statement

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,14 @@
+/* Multiple conditions can be addressed with if statement together with
+else if statement. */
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(7);
