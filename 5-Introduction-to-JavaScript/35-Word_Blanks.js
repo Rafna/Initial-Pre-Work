debc3c4c397589020ec88c8afab9f895d8533d9f
Rafna <rafnabash@gmail.com>
2019-07-15 01:11:35 -0600

    Show concatenation operation of strings using function

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+/* Here is an example of creating a new sentence with concatenation
+operator. */
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myNoun + " is " + myAdjective + ".He " + myVerb + " to me " + myAdverb + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
