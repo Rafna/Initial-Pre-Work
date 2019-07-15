572bde75934e56346a614e1facf781d05f082bc0
Rafna <rafnabash@gmail.com>
2019-07-15 00:45:02 -0600

    Use concatenation operator to build a new string with strings and variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+/* Using concatenation operator +, we can insert one or more variables
+and strings to build a new string. */
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Rafna";
+var myStr = "My name is " + myName + " and I am well!";
