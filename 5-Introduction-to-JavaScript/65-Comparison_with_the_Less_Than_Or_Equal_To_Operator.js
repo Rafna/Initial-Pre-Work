63c44115286718066cabad1d4cbfef6d05d5b4da
Rafna <rafnabash@gmail.com>
2019-07-15 15:10:52 -0600

    Use less than or equal to operator

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+/* Less than or equal to operator <= compares values of two numbers
+are less than or equal and return true or false. It also converts datatypes
+while comparing. */
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
