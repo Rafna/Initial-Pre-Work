49daa5362113a606368e8c88c5acdf84dbffb5b9
Rafna <rafnabash@gmail.com>
2019-07-15 00:57:52 -0600

    Show string immutable

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+/* String are immutable that means they cannot be changed. Here the first
+character of myStr cannot be changed individually. It can be Changed
+only by changing the entire string. */
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
