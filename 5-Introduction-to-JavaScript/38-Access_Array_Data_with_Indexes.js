c5057fc7382fcf85835735eca579e9c478da601a
Rafna <rafnabash@gmail.com>
2019-07-15 01:18:16 -0600

    Access array data

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+//Array data can be accessed with bracket notation [].
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
