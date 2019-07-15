f52f6fdea1f4be3f776cdd442157d1ee91bd591f
Rafna <rafnabash@gmail.com>
2019-07-15 00:59:37 -0600

    Find third character of a string

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//Another example of finding the character of a string at a specific index.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
