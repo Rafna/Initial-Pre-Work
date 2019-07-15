87bdac15ba936908df58095ba63689332cd3b24f
Rafna <rafnabash@gmail.com>
2019-07-15 01:30:38 -0600

    Use pop function

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+//Data can be removed from the end of an array using pop() function.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
