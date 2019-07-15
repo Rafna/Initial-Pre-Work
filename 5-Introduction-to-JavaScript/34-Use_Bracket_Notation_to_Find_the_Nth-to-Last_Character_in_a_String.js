da61f1500e7f20c12aec526ed203367ce5c88cb8
Rafna <rafnabash@gmail.com>
2019-07-15 01:05:17 -0600

    Find character of a string using length property

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//Another example of finding character using .length property of string.
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
