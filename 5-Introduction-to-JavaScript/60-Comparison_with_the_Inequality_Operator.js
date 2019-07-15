d716829559f7a5bcf384e1c68d417516b2981188
Rafna <rafnabash@gmail.com>
2019-07-15 14:54:11 -0600

    Use inequality operator

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+/* Inequality operator != is the opposite of equality operator. It also performs
+data type conversion while comparing. */
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
