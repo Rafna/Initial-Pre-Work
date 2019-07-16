a5814161fe11fb34558ca784a39ce4668b643dfe
Rafna <rafnabash@gmail.com>
2019-07-15 22:16:52 -0600

    Iterate using do while loop

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,11 @@
+/*Iterate using do-while loop. This is used when the code should be execute
+atleast once. */
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++;
+} while(i < 5);
