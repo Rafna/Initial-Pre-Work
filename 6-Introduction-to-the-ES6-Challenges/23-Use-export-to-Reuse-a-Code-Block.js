441de3389b976ec4dab3e564552721abd061a57e
Rafna <rafnabash@gmail.com>
2019-07-16 10:37:25 -0600

    Use export statement to export variable

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,6 @@
+/* When we want some code like a function, or a variable, to be used in
+ another file, we must export it to import it into another file.
+*/
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
