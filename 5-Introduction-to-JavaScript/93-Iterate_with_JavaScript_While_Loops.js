282543d3e691866c651f97326aaa31fbc65a7b1e
Rafna <rafnabash@gmail.com>
2019-07-15 21:53:05 -0600

    Use while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+/*Iterate with while loop. Same code will be executed until the condition
+ in the while is true. */
+ // Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
