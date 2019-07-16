ff38b001185882240ccf61dbe43871b980473114
Rafna <rafnabash@gmail.com>
2019-07-16 10:43:32 -0600

    Use import statement to import all the contents of a file

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,5 @@
+/* If we want all of the contents of a file imported to another file, use
+'*' with import statement. Also mention an object name to receive all the
+contents. */
+"use strict";
+import * as capitalizeString from "capitalize_strings";
