65406e4917a51bf624486531e3d37d2dfe98b3cc
Rafna <rafnabash@gmail.com>
2019-07-15 19:43:02 -0600

    Show object can handle flexible data

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,28 @@
+/*JavaScript object can handle flexible data. They allow for arbitrary
+combinations of strings, numbers, booleans, arrays, functions, and objects.
+*/
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+{
+    "artist": "The Beatles",
+    "title": "Abbey Road",
+    "release_year": 2009,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+];
