5ad005d1ed58ee5fd498438341b3d5be22449f6c
Rafna <rafnabash@gmail.com>
2019-07-16 10:30:20 -0600

    Use import statement

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+/* import statement is used to import functions from file. Earlier import
+statement imports all the functions and code in the file but now we can import
+only the function we want from the file. */
+import { capitalizeString } from "string_functions";
+"use strict";
+capitalizeString("hello!");
