bb1d6b2f787e5f6fded64a955e59c073ac721b0a
Rafna <rafnabash@gmail.com>
2019-07-15 21:56:09 -0600

    Iterate using for loop

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+//Iterate using for loop.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var i=1; i<=5; i++) {
+  myArray.push(i);
+}
