a5b7dc979de540b0ad157124a29738d50795c479
Rafna <rafnabash@gmail.com>
2019-07-16 02:03:03 -0600

    Write concise object literal declaration using simple fields

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,9 @@
+/* getMousePosition is a simple function that returns an object
+containing two fields.*/
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  const getMousePosition = (name,age,gender) => ({name,age,gender});
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
